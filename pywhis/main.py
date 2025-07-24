6b8ac0e30814959ff643fc5b8ddf90e7d69d85aa
John Otwell <john.otwell@proton.me>
2025-07-24 01:19:36 -0400

    change groq model since old is being retired

--- pywhis/main.py
+++ pywhis/main.py
@@ -28,7 +28,7 @@ def do_to_files_in_dir(directory: str, f: Callable[[BinaryIO], None]):
 def transcribe_audio(audio_file, destination_dir):
     file_name = os.path.basename(audio_file.name)
     client = Groq()
-    transcript = client.audio.transcriptions.create(model="distil-whisper-large-v3-en", file=audio_file)
+    transcript = client.audio.transcriptions.create(model="whisper-large-v3-turbo", file=audio_file)
 
     with open(os.path.join(destination_dir, file_name + ".md"), "w") as text_file:
         text_file.write(transcript.text)
